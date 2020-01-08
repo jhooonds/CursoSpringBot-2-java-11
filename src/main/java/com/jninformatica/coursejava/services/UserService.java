5a9659a13395aa057ecf092ebb84e1111a390c14
Jonatan Silva <41707797+jhooonds@users.noreply.github.com>
2020-01-08 00:52:13 -0300

    Service layer, component registration

---
+++ src/main/java/com/jninformatica/coursejava/services/UserService.java
@@ -0,0 +1,28 @@
+package com.jninformatica.coursejava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jninformatica.coursejava.entities.User;
+import com.jninformatica.coursejava.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	//Declarando dependencia para o repositorio, assim colocando
+	// @AutoWired para que o spring coloque a depéncia do Repository
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
