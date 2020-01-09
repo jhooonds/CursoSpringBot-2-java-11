ca2fb8fdc7d837ffd025b183c744ab79af1523f7
Jonatan Silva <41707797+jhooonds@users.noreply.github.com>
2020-01-08 23:27:30 -0300

    OrderStatus enum

--- src/main/java/com/jninformatica/coursejava/entities/Order.java
+++ src/main/java/com/jninformatica/coursejava/entities/Order.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.jninformatica.coursejava.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,6 +27,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	//Anotação para instruir o Framework a transformar esse campo em chave estrangeira
 	//Assim resolvendo o  problema de muitos para 1 (vários pedidos para o mesmo cliente)
 	@ManyToOne
@@ -36,9 +38,10 @@ public class Order implements Serializable{
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -64,6 +67,16 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
